830a71f39a7d1517b64ff457db1f8cde9cfc441a
alantao5056 <alantaolr@outlook.com>
2023-11-16 22:36:16 -0500

    new data

--- main.R
+++ main.R
@@ -3,6 +3,12 @@ library(tidyverse)
 public_schools = read_csv("./data/public_schools.csv")
 private_schools = read_csv("./data/private_schools.csv")
 school_scores = read_csv("./data/school_scores.csv")
+ma_schools = read_csv("./data/MA_schools.csv")
+
+temp = ma_schools |>
+  select("Average SAT_Reading", "Average SAT_Writing", "Average SAT_Math", "% Dropped Out", "Total Expenditures") |>
+  drop_na() |>
+  mutate("Average SAT" = `Average SAT_Reading` + `Average SAT_Writing` + `Average SAT_Math`).
 
 public_schools = public_schools |>
   select(NAME, CITY, STATE, POPULATION, ENROLLMENT, FT_TEACHER) |>
